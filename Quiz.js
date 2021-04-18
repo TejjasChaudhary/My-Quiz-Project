20b6e603b4d4267f470dee5eb75457e01b22e585
TejjasChaudhary <71205735+TejjasChaudhary@users.noreply.github.com>
2021-04-18 15:58:17 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,52 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    this.input = createInput("Name");
+    this.button = createButton("SUBMIT");   
+    this.title = createElement('h2',"MYQUIZ GAME");
+
+     //write code to change the background color here
+    background("pink");
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
